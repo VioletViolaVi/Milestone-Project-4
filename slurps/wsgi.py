c948f2e762318f0e18aaef403e51baeed0ba7bf2
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-02 09:42:26 +0000

    Initial commit.

---
+++ slurps/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for slurps project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slurps.settings')
+
+application = get_wsgi_application()
