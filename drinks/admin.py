b44fb46a91f6166aab446ae3079b6355198591c3
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-06 15:00:43 +0000

    Add columns to admin page to display field data specified in the json files.

--- drinks/admin.py
+++ drinks/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Drink_type, Drink
 
 # registers models to admin
-admin.site.register(Drink_type)
-admin.site.register(Drink)
+
+
+class Drink_type_admin(admin.ModelAdmin):
+    list_display = (
+        "programmatic_name",
+        "friendly_name",
+    )
+
+
+class Drink_admin(admin.ModelAdmin):
+    list_display = (
+        "image",
+        "drink_name",
+        "price",
+        "millilitres",
+        "drink_type",
+    )
+
+
+admin.site.register(Drink_type, Drink_type_admin)
+admin.site.register(Drink, Drink_admin)
--- drinks/models.py
+++ drinks/models.py
@@ -13,13 +13,12 @@ class Drink_type(models.Model):
 
 
 class Drink(models.Model):
-    drink_type = models.ForeignKey("Drink_type", null=True, blank=True, on_delete=models.SET_NULL)
     image = models.ImageField()
     drink_name = models.CharField(max_length=300)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     millilitres = models.IntegerField()
-    image_name = models.CharField(max_length=300, null=True, blank=True)
-    url = models.URLField(max_length=1024, null=True, blank=True)
+    drink_type = models.ForeignKey(
+        "Drink_type", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.drink_name
