91d7766d381e5ed71a256fad2ac213e1ff11014e
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-08 23:12:30 +0000

    Add four more drink images and sets of data to website.

--- drinks/views.py
+++ drinks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 
-# def all_drinks(request):
-#     # shows all drink options and search queries
-#     return render(request, "home/index.html", context)
+def all_drinks(request):
+    # shows all drink options and search queries
+    return render(request, "home/index.html", context)
