00a10da4885da232b46b6a91843b86c45bb155b1
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-10 19:07:52 +0000

    Change style of search bar.

--- home/views.py
+++ home/views.py
@@ -24,13 +24,18 @@ def home(request):
             sort = sort_key
             sort_key == "price"
 
-        if sort_key == "drink_name":
-            sort_key == "lower_case_name"
-            new_drinks = new_drinks.annotate(
-                lower_case_name=Lower("drink_name"))
-            juices = juices.annotate(lower_case_name=Lower("drink_name"))
-            milkshakes = milkshakes.annotate(
-                lower_case_name=Lower("drink_name"))
+            if sort_key == "drink_name":
+                sort_key == "lower_case_name"
+                new_drinks = new_drinks.annotate(
+                    lower_case_name=Lower("drink_name"))
+                juices = juices.annotate(lower_case_name=Lower("drink_name"))
+                milkshakes = milkshakes.annotate(
+                    lower_case_name=Lower("drink_name"))
+
+            # if sort_key == "drink_name":
+            #     sort_key == "lower_case_name"
+            #     searchable_drinks = searchable_drinks.annotate(
+            #         lower_case_name=Lower("drink_name"))
 
             if "direction" in request.GET:
                 direction = request.GET["direction"]
@@ -39,13 +44,14 @@ def home(request):
             new_drinks = new_drinks.order_by(sort_key)
             juices = juices.order_by(sort_key)
             milkshakes = milkshakes.order_by(sort_key)
+            # searchable_drinks = searchable_drinks.order_by(sort_key)
 
         if "search" in request.GET:
             drink_search = request.GET["search"]
             drink_search_results = searchable_drinks.filter(
                 Q(drink_name__icontains=drink_search))
 
-    price_sorting = f"{sort}_{direction}"
+    drink_sorting = f"{sort}_{direction}"
 
     context = {
         "new_drinks": new_drinks,
@@ -53,7 +59,7 @@ def home(request):
         "milkshakes": milkshakes,
         "drink_search_results": drink_search_results,
         "typed_in_search": drink_search,
-        "price_sorting": price_sorting,
+        "drink_sorting": drink_sorting,
     }
 
     # shows homepage
