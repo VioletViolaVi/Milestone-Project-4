ddaf98a34e09eef80a508709912541c61884a06d
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-10 13:26:25 +0000

    Add functionality to sort button so prices can be shown in ascending and descending order.

--- home/views.py
+++ home/views.py
@@ -13,12 +13,31 @@ def home(request):
     drink_search = None
     drink_search_results = ""
 
+    sort = None
+    direction = None
+
     if request.GET:
+
+        if "sort" in request.GET:
+            sort_key = request.GET["sort"]
+            sort = sort_key
+            sort_key == "price"
+
+            if "direction" in request.GET:
+                direction = request.GET["direction"]
+                if direction == "desc":
+                    sort_key = f"-{sort_key}"
+            new_drinks = new_drinks.order_by(sort_key)
+            juices = juices.order_by(sort_key)
+            milkshakes = milkshakes.order_by(sort_key)
+
         if "search" in request.GET:
             drink_search = request.GET["search"]
-
             drink_search_results = searchable_drinks.filter(
                 Q(drink_name__icontains=drink_search))
+
+    price_sorting = f"{sort}_{direction}"
+    alphabetical_sorting = f"{sort}_{direction}"
 
     context = {
         "new_drinks": new_drinks,
@@ -26,6 +45,8 @@ def home(request):
         "milkshakes": milkshakes,
         "drink_search_results": drink_search_results,
         "typed_in_search": drink_search,
+        "price_sorting": price_sorting,
+        "alphabetical_sorting": alphabetical_sorting,
     }
 
     # shows homepage
