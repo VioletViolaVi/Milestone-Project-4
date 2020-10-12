e091c785f00f1d52f6a98a8dc0f7822a9fe82be2
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-12 13:19:32 +0000

    Enable drink items to be stored in the shopping cart.

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.shopping_cart, name="shopping_cart"),
+    path('add/<item_id>/', views.add_to_shopping_cart,
+         name="add_to_shopping_cart"),
 ]
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def shopping_cart(request):
 
     # shows items in shopping cart
     return render(request, "shopping_cart/shopping_cart.html")
+
+
+def add_to_shopping_cart(request, item_id):
+    # add quantity of specified drink to cart
+
+    drink_quantity = int(request.POST.get("drinkSelections"))
+    redirect_url = request.POST.get("redirect_url")
+    shopping_cart = request.session.get("shopping_cart", {})
+
+    if item_id in list(shopping_cart.keys()):
+        shopping_cart[item_id] += drink_quantity
+    else:
+        shopping_cart[item_id] = drink_quantity
+
+    request.session["shopping_cart"] = shopping_cart
+    print(request.session["shopping_cart"])
+    return redirect(redirect_url)
