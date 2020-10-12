c8916819f3d49286fe6072854450c4cae62478fe
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-12 20:12:03 +0000

    Style new stickers for juices and milkshakes in the same way the new stickers for the new drinks were placed, position wise.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  // smooth scroll to screen top
+  // smooth scroll to screen top using arrow
   $(".back-to-top-link").click(function () {      
     window.scrollTo({
       top: 0,
