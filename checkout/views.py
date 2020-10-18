4860fb127540032df392dc1deb772d9dc8492c27
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-18 15:56:05 +0000

    Add stripe's card input to payment form.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         'drink_order_form': drink_order_form,
+        "stripe_public_key": "pk_test_51HUF24FyWkzyv3XJCItWmCeUd8D0vZOhRger4iVLVRZQShSmBC8H35voHb2Pj1LJ7Co5YGX4bAcLEzBk64Ome0og00jDR0YrzD",
+        "client_secret": "test client secret",
     }
 
     return render(request, template, context)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -21,4 +21,28 @@ $(document).ready(function () {
     let editForm = $(this).parent();
     editForm.submit();
   });
+
+  // stripe elements
+  let stripe_public_key = $("#id_stripe_public_key").text().slice(1, -1);
+  let client_secret = $("#id_client_secret").text().slice(1, -1);
+  let stripe = Stripe(stripe_public_key);
+  let elements = stripe.elements();
+  let style = {
+    base: {
+      color: "#212529",
+      fontFamily: "sans-serif",
+      src: "local('Montserrat Regular'), local('Montserrat-Regular'), url(https://fonts.gstatic.com/s/montserrat/v15/JTUSjIg1_i6t8kCHKm459WRhyyTh89ZNpQ.woff2) format('woff2')",//delete if not used
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#495057",
+      },
+    },
+    invalid: {
+      color: "red",
+      iconColor: "red",
+    },
+  };
+  let card = elements.create("card", { style: style });
+  card.mount("#stripeCard");
 });
