d35d4684bd31ad3e8c5e813756a19c7e102a1cc0
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-23 22:34:21 +0000

    Edit style to input fields on the update form.

--- user_profiles/forms.py
+++ user_profiles/forms.py
@@ -25,5 +25,5 @@ class UserProfilesForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs["placeholder"] = placeholder
-            self.fields[field].widget.attrs["class"] = "stripe-style-input"
+            self.fields[field].widget.attrs["class"] = "profile-countries"
             self.fields[field].label = False
