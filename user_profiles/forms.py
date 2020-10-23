f25f75c95bbb6d68b08138344b263c03bfb32318
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-23 20:13:22 +0000

    Add update form to profile page.

---
+++ user_profiles/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import UserProfiles
+
+
+class UserProfilesForm(forms.ModelForm):
+    class Meta:
+        model = UserProfiles
+        exclude = ("user",)
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            "default_phone_number": "Phone Number",
+            "default_street_address1": "Street Address 1",
+            "default_street_address2": "Street Address 2",
+            "default_postcode": "Postal Code",
+        }
+
+        self.fields["default_phone_number"].widget.attrs["autofocus"] = True
+        for field in self.fields:
+            if field != "default_country":
+                if self.fields[field].required:
+                    placeholder = f"{placeholders[field]} *"
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs["placeholder"] = placeholder
+            self.fields[field].widget.attrs["class"] = "stripe-style-input"
+            self.fields[field].label = False
--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfiles
+from .forms import UserProfilesForm
 
 
 def user_profiles(request):
     # displays user profiles
     user_profiles = get_object_or_404(UserProfiles, user=request.user)
+
+    form = UserProfilesForm(instance=user_profiles)
+    drink_orders = user_profiles.drink_orders.all()
+
     template = "user_profiles/user_profiles.html"
     context = {
-        "user_profiles": user_profiles,
+        "form": form,
+        "drink_orders": drink_orders,
     }
 
     return render(request, template, context)
