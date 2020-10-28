6cd4a5f0e64b290adf240925719c2c95b0294a82
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-28 21:14:25 +0000

    Exchange checkout history button for profiles button and remove border from image field on add page.

--- home/widgets.py
+++ home/widgets.py
@@ -4,4 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomClearableFileInput(ClearableFileInput):
     input_text = _("")
-    template_name = "home/custom_widget_templates/custom_clearable_file_input.html"
+    template_name = \
+        "home/custom_widget_templates/custom_clearable_file_input.html"
