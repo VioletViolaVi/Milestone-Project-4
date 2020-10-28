aa1a82e576e7817bd7c738315464d618bddeef6d
VioletViolaVi <vivian.v18723@gmail.com>
2020-10-28 22:48:08 +0000

    Add message for selected images in edit and add drink pages.

--- home/widgets.py
+++ home/widgets.py
@@ -1,8 +1,6 @@
 from django.forms.widgets import ClearableFileInput
-from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
-    input_text = _("")
     template_name = \
         "home/custom_widget_templates/custom_clearable_file_input.html"
