c31fd00fa37868bba6f484c2b69c5f4ff5ca8ba4
VioletViolaVi <vivian.v18723@gmail.com>
2020-11-08 15:29:21 +0000

    Edit contents of nav bar.

--- slurps/settings.py
+++ slurps/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oosunn#08&_1znt7t4+$+36=r1sfy_p=@0cfaz2&$u!1nra7$4'
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
