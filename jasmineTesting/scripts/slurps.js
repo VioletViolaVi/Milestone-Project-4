ed1aed623656ab5a256fe8575b2ad03d8bbad21c
VioletViolaVi <vivian.v18723@gmail.com>
2020-11-13 16:19:53 +0000

    Add jasmine testing for the project.

---
+++ jasmineTesting/scripts/slurps.js
@@ -0,0 +1,120 @@
+function scrollToTop(topScroll) {
+	if (typeof topScroll == "number") {
+		return topScroll;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function scrollBehaviour(behaviourScroll) {
+	if (typeof behaviourScroll == "string") {
+		return behaviourScroll;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function windowsScrollTop(windowsTopScroll) {
+    let windowsScrollTop = 500;
+	if (windowsTopScroll > 300) {
+		return windowsTopScroll;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function stripePublicKeySlices(slices) {
+	if (typeof slices == "number") {
+		return slices;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function baseColor(color) {
+	if (typeof color == "string") {
+		return color;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function baseTextTransform(textTransform) {
+	if (typeof textTransform == "string") {
+		return textTransform;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function baseFontFamily(fontFamily) {
+	if (typeof fontFamily == "string") {
+		return fontFamily;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function baseFontSmoothing(fontSmoothing) {
+	if (typeof fontSmoothing == "string") {
+		return fontSmoothing;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function baseFontSize(fontSize) {
+	if (typeof fontSize == "string") {
+		return fontSize;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function InvalidColor(colorAndIconColor) {
+	if (typeof colorAndIconColor == "string") {
+		return colorAndIconColor;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function formDisabled(disabled) {
+	if (typeof disabled == "boolean") {
+		return disabled;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function formFadeToggle(fadeToggle) {
+	if (typeof fadeToggle == "number") {
+		return fadeToggle;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function postData(postDataContents) {
+	if (typeof postDataContents == "string") {
+		return postDataContents;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function url(urlLink) {
+	if (typeof urlLink == "string") {
+		return urlLink;
+	} else {
+		return "ERROR!";
+	}
+}
+
+function paymentIntentStatus(paymentIntent) {
+	if (typeof paymentIntent == "string") {
+		return paymentIntent;
+	} else {
+		return "ERROR!";
+	}
+}
---
+++ jasmineTesting/spec/slurpsSpec.js
@@ -0,0 +1,136 @@
+describe("Slurps", function() {
+
+    describe("Check to see if scrollToTop equates to the number 0.", function() {
+		it("It should be 0.", function() {
+			expect(scrollToTop(0)).toBe(0);
+		});
+		it("It should return an error if not supplied with a number.", function() {
+			expect(scrollToTop("Hello World!")).toBe("ERROR!");
+		});
+	});
+
+	describe("Check to see if scrollBehaviour equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(scrollBehaviour("smooth")).toBe("smooth");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(scrollBehaviour(0123)).toBe("ERROR!");
+		});
+	});
+
+	describe("Check to see if windowsScrollTop is more than 300.", function() {
+		it("It should be more than 300.", function() {
+			expect(windowsScrollTop(500)).toBeGreaterThan(300);
+		});
+		it("It should return an error if the value is not more than 300.", function() {
+			expect(windowsScrollTop(200)).toBe("ERROR!");
+		});
+	});
+
+    describe("Check to see if stripePublicKeySlices equates to 1 or -1.", function() {
+		it("It should be 1 or -1.", function() {
+			expect(stripePublicKeySlices(1 | -1)).toBe(1 | -1);
+		});
+		it("It should return an error if not 1 or -1.", function() {
+			expect(stripePublicKeySlices("Hello World!")).toBe("ERROR!");
+		});
+	});
+
+	describe("Check to see if baseColor equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(baseColor("#212529" || "#6c757d")).toBe("#212529" || "#6c757d");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(baseColor(0123)).toBe("ERROR!");
+		});
+	});
+
+	describe("Check to see if baseTextTransform equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(baseTextTransform("capitalize")).toBe("capitalize");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(baseTextTransform(0123)).toBe("ERROR!");
+		});
+	});
+
+	describe("Check to see if baseFontFamily equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(baseFontFamily("Montserrat, sans-serif")).toBe("Montserrat, sans-serif");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(baseFontFamily(0123)).toBe("ERROR!");
+		});
+    });
+
+	describe("Check to see if baseFontSmoothing equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(baseFontSmoothing("antialiased")).toBe("antialiased");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(baseFontSmoothing(0123)).toBe("ERROR!");
+		});
+    });
+
+	describe("Check to see if baseFontSize equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(baseFontSize("16px" || "::placeholder")).toBe("16px" || "::placeholder");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(baseFontSize(0123)).toBe("ERROR!");
+		});
+    });
+
+	describe("Check to see if InvalidColor equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(InvalidColor("red")).toBe("red");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(InvalidColor(0123)).toBe("ERROR!");
+		});
+    });
+
+	describe("Check to see if formDisabled is a boolean.", function() {
+		let formDisabled = true || false;
+		it("It should be a boolean.", function() {
+			expect(formDisabled).toBe(true || false);
+		});
+	});
+
+    describe("Check to see if formFadeToggle equates to the number 100.", function() {
+		it("It should be 100.", function() {
+			expect(formFadeToggle(100)).toBe(100);
+		});
+		it("It should return an error if not supplied with a number.", function() {
+			expect(formFadeToggle("Hello World!")).toBe("ERROR!");
+		});
+	});
+
+	describe("Check to see if postData equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(postData("csrfmiddlewaretoken" || "client_secret" || "save_info")).toBe("csrfmiddlewaretoken" || "client_secret" || "save_info");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(postData(0123)).toBe("ERROR!");
+		});
+    });
+
+	describe("Check to see if url equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(url("/payment/cache_payment_data/")).toBe("/payment/cache_payment_data/");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(url(0123)).toBe("ERROR!");
+		});
+    });
+
+	describe("Check to see if paymentIntentStatus equates to a string.", function() {
+		it("It should be a string.", function() {
+			expect(paymentIntentStatus("succeeded")).toBe("succeeded");
+		});
+		it("It should return an error if not supplied with a string.", function() {
+			expect(paymentIntentStatus(0123)).toBe("ERROR!");
+		});
+    });
+
+});
