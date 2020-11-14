ed058b96b8519c5b02cd8aa6047140e0ff1c500e
VioletViolaVi <vivian.v18723@gmail.com>
2020-11-14 18:34:00 +0000

    Add more unit tests to about us app.

--- about_us/tests_forms.py
+++ about_us/tests_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import About_us
 from .widgets import CustomClearableFileInput
+from .forms import AboutUsForm
 
 
 class TestForms(TestCase):
@@ -19,3 +20,33 @@ class TestForms(TestCase):
     def test_image_field_widget(self):
         widget = CustomClearableFileInput
         self.assertIs(widget, CustomClearableFileInput)
+
+    def test_about_us_form_image_and_image_description_are_not_required(self):
+        form = AboutUsForm({"title": "Test",
+                            "paragraph": "Test",
+                            "section": "Test",
+                            })
+        self.assertFalse(form.is_valid())
+
+    def test_about_us_form_title_is_required(self):
+        form = AboutUsForm({"title": ""})
+        self.assertFalse(form.is_valid())
+        self.assertIn("title", form.errors.keys())
+        self.assertEqual(form.errors["title"][0], "This field is required.")
+
+    def test_about_us_form_paragraph_is_required(self):
+        form = AboutUsForm({"paragraph": ""})
+        self.assertFalse(form.is_valid())
+        self.assertIn("paragraph", form.errors.keys())
+        self.assertEqual(form.errors["paragraph"][0],
+                         "This field is required.")
+
+    def test_about_us_form_section_is_required(self):
+        form = AboutUsForm({"section": ""})
+        self.assertFalse(form.is_valid())
+        self.assertIn("section", form.errors.keys())
+        self.assertEqual(form.errors["section"][0], "This field is required.")
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = AboutUsForm()
+        self.assertEqual(form.Meta.fields, "__all__")
