ad2c78dd34840b29b869b740ae840ed05467218b
VioletViolaVi <vivian.v18723@gmail.com>
2020-11-22 19:18:41 +0000

    Edit settings file in attempt to deploy app.

--- slurps/settings.py
+++ slurps/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-import dj_database_url
 from pathlib import Path
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
