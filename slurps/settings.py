4b7857314b6abdff001d1ce748e76d5a0ddff6c6
VioletViolaVi <vivian.v18723@gmail.com>
2020-11-23 10:16:19 +0000

    Make debug true only when there is a variable called development.

--- slurps/settings.py
+++ slurps/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["milestone-project-4-vivian.herokuapp.com", "localhost"]
 
